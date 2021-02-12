5c457ae93b2620df55b112c7684ed3e79e5238d9
Dundubi <73653183+dundubi@users.noreply.github.com>
2021-02-12 13:59:13 +0530

    Add files via upload

---
+++ automate_a_task.py
@@ -0,0 +1,7 @@
+Question = input("Enter the task : ")
+
+if Question == "Brush your teeth" :
+    print("You have to do this task at 9:00 am")
+
+if Question == "Complete your Hometask" :
+    print("You have to do this task after all your school classes")
